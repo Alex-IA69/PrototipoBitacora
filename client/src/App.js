dede6f25c817ad915ce9ef638d7df8aaa2735216
Alex-IA69 <A01635557@tec.mx>
2023-06-04 21:22:30 -0600

    Corrección conexión front-back

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App(){
+const App = () => {
+  const handleGet = () => {
+    axios.get('/sesiones')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-  const [backendData, setBackendData] = useState([{}])
+  const handlePost = () => {
+    const datetime = new Date(); // Fecha y Hora actual
+    const postData = {
+      cantParticipantes : 5,
+      nombreAct : 'Actividad de Prueba',
+      nombreEspacio : 'Espacio Meiquer',
+      fecha : datetime.toISOString()
+    };
 
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+    axios.post('/sesiones', postData)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handlePatch = () => {
+    axios.patch('/sesiones/4')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleDelete = () => {
+    axios.delete('/sesiones/5')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
+      <h1>API Buttons</h1>
+      <button onClick={handleGet}>Get Data</button>
+      <button onClick={handlePost}>Create Data</button>
+      <button onClick={handlePatch}>Update Data</button>
+      <button onClick={handleDelete}>Delete Data</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
